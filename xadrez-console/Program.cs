e1305ebc1191ab52186a209d7e910acd7dbb58de
vinimarks14@gmail.com <vinimarks14@gmail.com>
2023-03-15 11:55:59 -0300

    Começando a implementar um método que imprime o tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,6 +8,8 @@ namespace xadrez_console
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8,8);
+
+            Tela.ImprimirTabuleiro(tab);
         }
     }
 }
